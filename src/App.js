aef96eaab7dbd3eb2ccd1757db3d2bc9cef54705
Olmeksky <Olmeksky@gmail.com>
2024-11-05 06:10:32 +0300

    начальная

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
       </header>
     </div>
+    /*
+    <div className="container">
+      <Button variant="success">Success</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="danger">Danger</Button>
+      <Alert key="1" variant="success">This is a success alert—check it out!</Alert>
+    </div>*/
   );
 }
 
